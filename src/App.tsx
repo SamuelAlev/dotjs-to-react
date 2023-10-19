0056a43e44dde49f311dbd732fd1ef3ad75cd810
Samuel Alev <samuel.alev@frontify.com>
2023-10-19 19:44:41 +0200

    feat: add a bit of error handling

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,15 @@ import 'react18-json-view/src/style.css';
 
 export const App = () => {
     const shikiRef = useRef<HighlighterCore | null>(null);
-    const [content, setContent] = useState('');
+    const [content, setContent] = useState(localStorage.getItem('content') ?? '');
+    const [astContent, setAstContent] = useState({});
     const [tsxContent, setTsxContent] = useState('');
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         const generatePreview = async () => {
+            setIsError(false);
+
             if (!content) {
                 return;
             }
@@ -30,12 +34,19 @@ export const App = () => {
                 });
             }
 
-            const prettifiedCode = await prettier.format(DotJsAst.toJsxString(content), {
-                parser: 'babel-ts',
-                plugins: [babelPlugin, tsPlugin, estreePlugin],
-            });
-            const code = shikiRef.current.codeToHtml(prettifiedCode, { lang: 'tsx', theme: 'vitesse-light' });
-            setTsxContent(code);
+            try {
+                setAstContent(DotJsAst.parse(content));
+
+                const prettifiedCode = await prettier.format(DotJsAst.toJsxString(content), {
+                    parser: 'babel-ts',
+                    plugins: [babelPlugin, tsPlugin, estreePlugin],
+                });
+                const code = shikiRef.current.codeToHtml(prettifiedCode, { lang: 'tsx', theme: 'vitesse-light' });
+                setTsxContent(code);
+                setIsError(false);
+            } catch (error) {
+                setIsError(true);
+            }
         };
 
         generatePreview().catch(console.error);
@@ -54,18 +65,30 @@ export const App = () => {
             </div>
 
             <Split className="h-full overflow-auto">
-                <textarea
-                    className="flex w-[40%] overflow-auto p-4 font-mono focus-within:outline-none"
-                    placeholder="Enter the doT code"
-                    onInput={(event) => setContent((event.target as HTMLTextAreaElement).value)}
-                ></textarea>
+                <div className="relative flex w-[40%] overflow-auto">
+                    <textarea
+                        className="flex w-full p-4 font-mono focus-within:outline-none"
+                        placeholder="Enter the doT code"
+                        defaultValue={content}
+                        onInput={(event) => {
+                            setContent((event.target as HTMLTextAreaElement).value);
+                            localStorage.setItem('content', (event.target as HTMLTextAreaElement).value);
+                        }}
+                    ></textarea>
+
+                    {isError ? (
+                        <div className="bg-background absolute right-4 top-4 flex gap-2 rounded-lg border px-2 py-1">
+                            <div className="flex items-center justify-center">
+                                <div className="h-2 min-h-[8px] w-2 min-w-[8px] rounded-full bg-red-500"></div>
+                            </div>
+
+                            <span>Invalid doT.js template</span>
+                        </div>
+                    ) : null}
+                </div>
 
                 <Split mode="vertical" className="w-[60%]">
-                    <JsonView
-                        className="flex h-[50%] w-full overflow-auto p-4"
-                        src={content ? DotJsAst.parse(content) : {}}
-                        enableClipboard={false}
-                    />
+                    <JsonView className="flex h-[50%] w-full overflow-auto p-4" src={content ? astContent : {}} enableClipboard={false} />
 
                     {tsxContent ? (
                         <div
